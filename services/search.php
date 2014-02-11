3221cffd33b41d4a539b63f4fcbef443bd60a955
Carles Mateo <carles.mateo@gmail.com>
2014-02-11 01:53:53 +0100

    Search finished

---
+++ services/search.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Creator:      Carles Mateo
+ * Date:         2014-02-10 23:53
+ * Last Updater: Carles Mateo
+ * Last Updated: 2014-02-11 01:50
+ * Filename:     search.php
+ * Description:  Search Service
+ *               Test it with_
+ *               http://html.aeriatest.loc/search/description+author?q=zo
+ *               http://json.aeriatest.loc/search/?q=zo
+ */
+
+$o_books = new SmartXML(file_get_contents('../data/books.xml'));
+
+$s_subfilter = Router::$uri[1];
+$s_search = $_GET['q'];
+
+$st_valid_subfilters = Array('author', 'title', 'genre', 'price', 'publish_date', 'description');
+
+if ($s_subfilter == '') {
+    // No subfilters, that means search in all the scopes.
+    $st_subfilter = $st_valid_subfilters;
+} else {
+    $st_subfilter = explode('+', $s_subfilter);
+}
+
+// Check security
+$b_bad_scope = false;
+$s_xpath_filter = '';
+foreach($st_subfilter as $i_key=>$s_subfilter_to_check) {
+    if (!in_array($s_subfilter_to_check, $st_valid_subfilters, true)) {
+        $b_bad_scope = true;
+        break;  // Do not waste CPU
+    } else {
+        // Everything Ok, we build the query
+        if ($s_xpath_filter == '') {
+            $s_xpath_filter = '['.$s_subfilter_to_check.'[contains(.,\''.$s_search.'\')]';
+        } else {
+            $s_xpath_filter .= ' or ';
+            $s_xpath_filter .= $s_subfilter_to_check.'[contains(.,\''.$s_search.'\')]';
+        }
+    }
+}
+
+$s_xpath_filter .= ']';
+
+if ($b_bad_scope == false) {
+    // XPATH query construction
+    $s_xquery = '/catalog/book'.$s_xpath_filter;
+    // Example:
+    // /catalog/book[author[contains(.,'zombies')] or title[contains(.,'zombies')] or genre[contains(.,'zombies')] or price[contains(.,'zombies')] or publish_date[contains(.,'zombies')] or description[contains(.,'zombies')]]
+
+    // get results
+    $results = $o_books->xpath->query($s_xquery);
+
+    if($results->count())
+    {
+        echo '<results>';
+        foreach($results as $book)
+            echo $book."\n";
+        echo '</results>';
+    }
+    else
+    {
+        echo IO::error('no matching results');
+    }
+
+} else {
+    // If a wrong parameter is sent, we will return this
+    // Fe: http://html.aeriatest.loc/search/description+author+invented?q=zo
+    echo IO::error('wrong parameters');
+}
+
