a3ae305670a83d5145d49c3f1e55153f83e7acd8
Carles Mateo <carles.mateo@gmail.com>
2014-02-11 02:29:13 +0100

    Extra page for list

--- services/list.php
+++ services/list.php
@@ -6,6 +6,9 @@ $o_books = new SmartXML(file_get_contents('../data/books.xml'));
 $validFilters = array_flip(array('author','genre','price'));
 $filters = array_intersect_key($_GET,$validFilters);
 
+$i_num_results_per_page = 5;
+$s_page = (isset($_GET['page']) ? $_GET['page'] : '0');
+
 // XPATH query construction
 $xquery = '/catalog/book';
 $s_subfilter = '';
@@ -45,12 +48,33 @@ $xquery .= $s_subfilter;
 // get results
 $results = $o_books->xpath->query($xquery);
 
+$i_counter = 0;
+$i_page = 1;
+$b_page_results_printed = false;
 if($results->count())
 {
-	echo '<results>';
-	foreach($results as $book)
-		echo $book."\n";
-	echo '</results>';
+        echo '<results>'."\n";
+        foreach($results as $book) {
+            $i_counter++;
+            if ($i_counter > $i_num_results_per_page) {
+                $i_counter = 0;
+                $i_page++;
+            }
+
+            if (intval($s_page) == 0 || $i_page == intval($s_page)) {
+                $b_page_results_printed = true;
+
+                echo $book."\n";
+            } else {
+                // We don't have to print
+                // We check if we printed before, in that case do not waste more CPU on useless cycles
+                if ($b_page_results_printed == true) {
+                    break;
+                }
+            }
+
+        }
+        echo '</results>';
 }
 else
 {
